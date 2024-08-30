7559102840ca3e9ec4c63f44718a66c0006dd173
AKoivisto <antton.v.koivisto@student.jyu.fi>
2024-08-30 15:45:00 +0300

    2.18/2.20

--- osa2/maatiedot/src/App.jsx
+++ osa2/maatiedot/src/App.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/prop-types */
 import { useState, useEffect } from 'react'
 import countryService from './services/countries'
+
 
 const Filter = ({ft, handleFT}) => {
   return (
@@ -15,11 +17,87 @@ const Filter = ({ft, handleFT}) => {
   )
 
 }
-
+// const Languages = ({country}) => {
+//   return (
+//     <div>
+//       <h2>Languages:</h2>
+//       <div>
+//         {country.map(language => 
+//           <
+//         )}
+//       </div>
+//     </div>
+//   )
+// }
 const CountryLine = ({country}) => {
   return (
     <div>
-      {country} it is
+      <p>{country}</p>
+    </div>
+  )
+}
+
+
+const Weather = ({location}) => {
+  const [weather, setWeather] = useState(undefined)
+  const lat = location[0]
+  const lon = location[1]
+  
+  useEffect(() => {
+    countryService
+    .getWeather(lat,lon)
+    .then(response => {
+      setWeather(response.data)
+      console.log(weather)
+    })
+  },[])
+
+
+  if(!weather) {
+    return null
+  }
+
+  const temps = weather.main
+  const temp = Math.round((temps.temp - 272.15))
+  const wind = weather.wind.speed
+  console.log(temp)
+  const icon = weather.weather[0].icon
+  console.log(icon);
+  
+
+  return (
+    <div>
+    <p>temperature: {temp} C</p>
+    <p>wind: {wind} m/s</p>
+    <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} />
+    </div>
+  )
+}
+
+const CountryStats = ({country}) => {
+  console.log(country);
+  const languages = Object.values(country.languages)
+  console.log(languages)
+  //const langi = languages.map(language => language +'a' )
+  //console.log(langi)
+  
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <div>
+        <p>capital: {country.capital}</p>
+        <p>population: {country.population}</p>
+        <p>region: {country.region} </p>
+      </div>
+      <h2>Languages</h2>
+      <ul> 
+          {languages.map((language) => (
+            <li key={language}>{language}</li>
+          ))}
+      </ul>
+      <img src={country.flags.png}/>
+      <h3>Weather in {country.capital}</h3>
+      <Weather location={country.capitalInfo.latlng} />
     </div>
   )
 }
@@ -28,18 +106,23 @@ const CountryList = ({ft, countries}) => {
   //console.log('ft:',ft,countries);
 
   const countryList = countries.map(country => country.name.common)
-  console.log({countryList});
+  //console.log({countryList});
   const filteredList = countryList.filter((country) =>
     country.toLowerCase().includes(ft.toLowerCase())
 )
-  console.log({filteredList})
+  //console.log({filteredList})
   
-  const countriesInList = countries.filter(country => country.name.common.includes(ft))
-  console.log({countriesInList})
+  const countriesInList = countries.filter(country => country.name.common.toLowerCase().includes(ft.toLowerCase()))
+  //console.log({countriesInList})
 
   if (filteredList.length > 10) {
     return(
-      <p>Too many results.</p>
+      <p>Too many results. Please specify.</p>
+    )
+  }
+  if (filteredList.length === 1) {
+    return(
+      <CountryStats country={countriesInList[0]} />
     )
   }
   else {
@@ -67,7 +150,7 @@ function App() {
       .getAll()
       .then(response => {
         setCountries(response.data)
-        //console.log(countries);
+        console.log(countries);
         
       })
   }, [])
--- osa2/maatiedot/src/services/countries.js
+++ osa2/maatiedot/src/services/countries.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/name/'
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?'
+const api_key = import.meta.env.VITE_WAPI
 
 const getAll = () => {
     return axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
@@ -10,7 +12,13 @@ const getCountry = (country) => {
 
 }
 
+const getWeather = (lat,lon) => {
+    return axios.get(
+        `${weatherUrl}lat=${lat}&lon=${lon}&appid=${api_key}`)
+}
+
 export default {
     getAll,
-    getCountry
+    getCountry,
+    getWeather
 }
