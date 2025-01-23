6e38f5f9be40a828399dabb76049d9f80962a4fb
AKoivisto <antton.v.koivisto@student.jyu.fi>
2025-01-23 10:37:30 +0200

    6.12 - 6.13

--- osa6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
+import { showNotification } from '../reducers/notificationReducer'
 
 
 const AnecdoteForm = () => {
@@ -10,6 +11,10 @@ const AnecdoteForm = () => {
         const toAdd = event.target.new_anecdote.value
         event.target.new_anecdote.value = ''
         dispatch(createAnecdote(toAdd))
+        dispatch(showNotification(`added anecdote '${toAdd}'`))
+        setTimeout(() => {
+          dispatch(showNotification(''))
+        }, 5000)
       }
 
     return (
--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { showNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
     const dispatch = useDispatch()
@@ -11,7 +12,13 @@ const AnecdoteList = () => {
 
     const vote = (id) => {
         console.log('vote', id)
+        const votedAnecdote = anecdotes.find(anecdote => anecdote.id === id)
+        console.log(votedAnecdote)
         dispatch(voteAnecdote(id))
+        dispatch(showNotification(`you voted '${votedAnecdote.content}'`))
+        setTimeout(() => {
+          dispatch(showNotification(''))
+        }, 5000)
       }
 
     if (!anecdotes || anecdotes.length === 0) {
